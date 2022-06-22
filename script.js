296e5546a4366335079f2abe37e7585f24e03589
GiftedHands43 <100702359+GiftedHands43@users.noreply.github.com>
2022-06-22 21:50:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.write("Name: " + " " + " My name is Gift");
+document.write("<br>")
+document.write("Height: " + " " + " I am 5'8");
+document.write("<br>")
+document.write("Country: " + " " + " Nigeria");
